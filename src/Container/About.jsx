0266f1ac53101b38fd9af3908624f92afd9ee776
FizzyElt <a0979150560@gmail.com>
2020-02-07 22:42:20 +0800

    新增about內容

--- src/Container/About.jsx
+++ src/Container/About.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 
+import { Typography ,Divider} from 'antd/es'
+import './About.scss'
+const { Title, Paragraph, Text } = Typography
 const About = () => {
     return (
-        <div>
-            About
-        </div>
+       <Typography className="about-container">
+           <Title>
+                作品說明
+           </Title>
+           <Divider/>
+           <Title level={2}>框架</Title>
+           <Title level={4}>
+               React.js、Ant-Design(UI框架)
+           </Title>
+           <Title level={2}>
+                函示庫
+           </Title>
+           <Title level={4}>
+                React-Router、React-Transition-Group、Axios、React-Icons
+           </Title>
+           <Title level={2}>
+                開發工具
+           </Title>
+           <Title level={4}>
+                Webpack、SCSS
+           </Title>
+           <Title level={2}>
+                作品重點
+           </Title>
+           <Title level={4}>
+                自製Slider組件、Webpack設定檔撰寫、串接氣象局API
+           </Title>
+           <Title level={2}>
+                作品原始碼連結
+           </Title>
+           <Title level={4}>
+                <a href="https://github.com/FizzyElt/ReactWeatherWeb">https://github.com/FizzyElt/ReactWeatherWeb</a>
+           </Title>
+       </Typography>
     );
 }
 

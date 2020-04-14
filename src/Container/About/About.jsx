3ac0fc184e420b421768559269f84bc0bbc63bdc
FizzyElt <a0979150560@gmail.com>
2020-04-14 21:16:03 +0800

    圖表手機板修正

--- src/Container/About/About.jsx
+++ src/Container/About/About.jsx
@@ -3,6 +3,26 @@ import React from 'react';
 import { Typography ,Divider} from 'antd/es'
 import './About.scss'
 const { Title } = Typography
+
+const obj=[
+     {
+          title:"框架",
+          content:"React.js、Ant-Design(UI框架)"
+     },
+     {
+          title:"函示庫",
+          content:"React-Router、React-Transition-Group、Axios、React-Icons"
+     },
+     {
+          title:"開發工具",
+          content:"Webpack、SCSS"
+     },
+     {
+          title:"作品重點",
+          content:"自製Slider組件、Webpack設定檔撰寫、串接氣象局API"
+     },
+]
+
 const About = () => {
     return (
        <Typography className="about-container">
@@ -10,33 +30,17 @@ const About = () => {
                 作品說明
            </Title>
            <Divider/>
-           <Title level={2}>框架</Title>
-           <Title level={4}>
-               React.js、Ant-Design(UI框架)
-           </Title>
-           <Title level={2}>
-                函示庫
-           </Title>
-           <Title level={4}>
-                React-Router、React-Transition-Group、Axios、React-Icons
-           </Title>
-           <Title level={2}>
-                開發工具
-           </Title>
-           <Title level={4}>
-                Webpack、SCSS
-           </Title>
-           <Title level={2}>
-                作品重點
-           </Title>
-           <Title level={4}>
-                自製Slider組件、Webpack設定檔撰寫、串接氣象局API
-           </Title>
+           {obj.map(o=>{
+                return (<>
+                <Title level={2}>{o.title}</Title>
+                <Title level={4}>{o.content}</Title>
+                </>)
+           })}
            <Title level={2}>
                 作品原始碼連結
            </Title>
            <Title level={4}>
-                <a href="https://github.com/FizzyElt/ReactWeatherWeb">
+                <a href="https://github.com/FizzyElt/ReactWeatherWeb" target="_blank">
                      https://github.com/FizzyElt/ReactWeatherWeb
                      </a>
            </Title>
